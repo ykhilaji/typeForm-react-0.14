fc8729782d481db2253e8d4306835d0761f240be
KHILAJI YASSINE <ykhilaji@gmail.com>
2024-01-16 21:32:11 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TypeForm from "./TypeForm";
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <TypeForm />
+      </div>
+    );
+  },
+});
---
+++ src/components/TypeForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default React.createClass({
+  displayName: "TypeFomr",
+
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.src = "//embed.typeform.com/next/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+  },
+
+  componentWillUnmount() {
+    const script = document.querySelector(
+      'script[src="//embed.typeform.com/next/embed.js"]'
+    );
+    if (script) {
+      document.body.removeChild(script);
+    }
+  },
+
+  render() {
+    return (
+      <div
+        data-tf-widget="CZLpXYXT"
+        data-tf-opacity="100"
+        data-tf-iframe-props="title=Code Blend Form"
+        data-tf-transitive-search-params
+        data-tf-medium="snippet"
+        style={{ width: "100%", height: "500px" }}
+      ></div>
+    );
+  },
+});
